3f21a366358c47a298ac75a064d062391570daa0
blue <neelabalan.n@outlook.com>
2021-04-02 11:08:04 +0530

    segregating the dash server and xinput script #2

--- script/keystroke.py
+++ script/keystroke.py
@@ -2,18 +2,8 @@ import subprocess
 import argparse
 import os
 import datetime
-import dash
-import dash_daq as daq
-import pandas as pd
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.express as px
 from pathlib import Path
 from collections import Counter, OrderedDict
-from keymap import keymap
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 home = str(Path.home())
 logname =  str(datetime.datetime.now().date()) + 'keystrokes.log'
@@ -68,79 +58,8 @@ def run():
         print('EXCEPTION not valid device id')
 
 
-def filter_key_release(lines):
-    ''' filter key release string in lines '''
-    for line in lines:
-        return True if line.startswith('key press') else False
-
-
-def get_key_presses(contents):
-    ''' returns keycodes as list '''
-    lines = contents.splitlines()
-    filteredLines = filter(
-        lambda line: line.startswith('key press') is True, lines)
-    keypresses = [
-        keymap.get(
-            int(
-                remove_empty_str_from_list(
-                    keycode.split(' ')
-                )[-1]
-            )
-        ) for keycode in filteredLines
-    ]
-    return keypresses
-
-
-def get_contents():
-    with open(logpath, 'r') as file:
-        contents = file.read()
-        return contents
-
-
-def stats():
-    '''get stats in df'''
-    contents = get_contents()
-    keypresses = get_key_presses(contents)
-    freq_map = Counter(keypresses).most_common()
-    df = pd.DataFrame.from_dict(freq_map)
-    df.columns = ['keys', 'freq']
-    df.set_index('keys', inplace=True)
-    return df
-
-
-def serve_layout():
-    fig = px.bar(stats(), barmode="group")
-    return html.Div([
-        html.H1(
-            children='Total keystrokes',
-            style={ 'textAlign': 'center'}
-        ),
-        daq.LEDDisplay(
-            id='my-LED-display',
-            label="",
-            color="#103366",
-            value=stats()['freq'].sum(),
-            style={ 'textAlign': 'center'}
-        ),
-        html.Br(), html.Br(),
-        html.Div(children=[
-            html.H1(children='Keystroke graph', style={'textAlign': 'center'}),
-            dcc.Graph(
-                id='example-graph',
-                figure=fig
-            )
-        ])
-    ])
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-s',
-        '--stats',
-        action='store_true',
-        help='show the stats'
-    )
     parser.add_argument(
         '-r',
         '--run',
@@ -160,6 +79,3 @@ if __name__ == "__main__":
             run()
         elif args.kill:
             kill('xinput test')
-        elif args.stats:
-            app.layout = serve_layout
-            app.run_server(debug=True)
