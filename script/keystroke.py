70313c42a36cb96ceeda7fa415afd3cf1ade4650
blue <neelabalan.n@outlook.com>
2021-04-07 23:04:00 +0530

    code reformatted, transpose fix

--- script/keystroke.py
+++ script/keystroke.py
@@ -5,121 +5,119 @@ import datetime
 import os
 import sys
 from pathlib import Path
-from collections import Counter 
+from collections import Counter
 from sqlalchemy import create_engine
 from apscheduler.schedulers.background import BackgroundScheduler
 from keymap import keymap
 
-SCHEDULER_INTERVAL= 5
+SCHEDULER_INTERVAL = 5
 scheduler = BackgroundScheduler(daemon=True)
-engine = create_engine('sqlite:///{}/.keystroke/keystrokes.db'.format(str(Path.home())))
+engine = create_engine("sqlite:///{}/.keystroke/keystrokes.db".format(str(Path.home())))
+
 
 def get_xinput_ids():
     try:
         xinput_ids = subprocess.Popen(
             "xinput list | grep -Po 'id=\K\d+(?=.*slave\s*keyboard)'",
             shell=True,
-            stdout=subprocess.PIPE
+            stdout=subprocess.PIPE,
         ).communicate()[0]
-        return xinput_ids.decode('utf-8').replace('\n', ' ')
+        return xinput_ids.decode("utf-8").replace("\n", " ")
     except:
-        print('EXCEPTION xinput ID not found')
+        print("EXCEPTION xinput ID not found")
 
 
 def get_pids(output, target_process):
-    ''' get PID for running target process '''
+    """ get PID for running target process """
     filtered_process_list = [
-        str(process) for process in output.splitlines() if target_process in str(process)
+        str(process)
+        for process in output.splitlines()
+        if target_process in str(process)
     ]
     pids = list()
     for process in filtered_process_list:
-        process = list(filter(None, process.split(' ')))
+        process = list(filter(None, process.split(" ")))
         pids.append(process[1])
     return pids
 
 
 def kill(target_process):
-    ''' kills the xinput test process '''
+    """ kills the xinput test process """
     if target_process:
-        process = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+        process = subprocess.Popen(["ps", "aux"], stdout=subprocess.PIPE)
         output, error = process.communicate()
         pids = get_pids(output, target_process)
         for pid in pids:
-            print('killing PID {}'.format(pid))
+            print("killing PID {}".format(pid))
             os.kill(int(pid), 9)
 
+
 def read_buffer(buffer):
-    ''' read standard out buffer '''
-    return buffer.stdout.read1().decode('utf-8')
+    """ read standard out buffer """
+    return buffer.stdout.read1().decode("utf-8")
+
 
 def split_text(buffer_text: str):
-    ''' split text on \n '''
-    return buffer_text.split('\n')
+    """ split text on \n """
+    return buffer_text.split("\n")
+
 
 def filter_text(lines):
-    ''' filter empty strings and key releases '''
-    filtered_lines = filter(
-        lambda line: line.startswith('key press'), lines
-    )
+    """ filter empty strings and key releases """
+    filtered_lines = filter(lambda line: line.startswith("key press"), lines)
     return list(filter(None, filtered_lines))
 
+
 def map_keycode_to_keys(filtered_lines):
+    """ mapping xinput keycodes to keyboard keys """
     return [
-        keymap.get(
-            int(
-                list(
-                    filter(None, keycode.split(' '))
-                )[-1]
-            )
-        ) for keycode in filtered_lines
-    ] 
+        keymap.get(int(list(filter(None, keycode.split(" ")))[-1]))
+        for keycode in filtered_lines
+    ]
+
 
 def workflow(buffer):
-    ''' 
-    -> read from buffer 
-    -> transform text for easy filtering 
-    -> filter key releases and empty string 
+    """
+    -> read from buffer
+    -> transform text for easy filtering
+    -> filter key releases and empty string
     -> map xinput code to char
     -> transform to pandas dataframe and write to sqlite
-    '''
+    """
     sqlite_connection = engine.connect()
-    keypress = map_keycode_to_keys(
-            filter_text(
-                split_text(
-                    read_buffer(buffer)
-                )
-        )
-    )
+    keypress = map_keycode_to_keys(filter_text(split_text(read_buffer(buffer))))
     meta_dict = {
-        'timestamp': datetime.datetime.now(),
-        'total': len(keypress),
+        "timestamp": datetime.datetime.now(),
+        "total": len(keypress),
     }
     df = pd.DataFrame(
-        [
-            { **Counter(keypress), **meta_dict }
-        ], columns=list(keymap.values()) + list(meta_dict.keys())
+        [{**Counter(keypress), **meta_dict}],
+        columns=list(keymap.values()) + list(meta_dict.keys()),
     )
     print(df.head())
 
     if keypress:
-        df.to_sql('keystroke', sqlite_connection, if_exists='append')
+        df.to_sql("keystroke", sqlite_connection, if_exists="append")
     sqlite_connection.close()
 
 
 def run():
-    ''' run the app and log the keystrokes BufferedReader '''
+    """ run the app and log the keystrokes BufferedReader """
     echos = None
     try:
         command = "echo " + get_xinput_ids() + " | xargs -P0 -n1 xinput test"
         echos = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
     except:
-        print('EXCEPTION not valid device id')
+        print("EXCEPTION not valid device id")
 
     # testing
     # workflow(echos)
-    scheduler.add_job(workflow, trigger='interval', seconds=SCHEDULER_INTERVAL, args=(echos,))
+    scheduler.add_job(
+        workflow, trigger="interval", seconds=SCHEDULER_INTERVAL, args=(echos,)
+    )
     scheduler.start()
     import time
+
     try:
         while True:
             time.sleep(2)
@@ -129,9 +127,9 @@ def run():
 
 if __name__ == "__main__":
     arg = sys.argv[1]
-    if arg == '-r':
+    if arg == "-r":
         run()
-    elif arg == '-k':
-        kill('xinput test')
+    elif arg == "-k":
+        kill("xinput test")
     else:
-        print('wrong arguments provided')
+        print("wrong arguments provided")
--- server/app.py
+++ server/app.py
@@ -9,49 +9,55 @@ from sqlalchemy import create_engine
 
 # testing
 # engine = create_engine('sqlite:////home/blue/.keystroke/keystrokes.db')
-engine = create_engine('sqlite:////root/.keystroke/keystrokes.db')
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+engine = create_engine("sqlite:////root/.keystroke/keystrokes.db")
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
 def data_for_the_day():
-    ''' total keystrokes for the day from db '''
+    """ total keystrokes for the day from db """
     with engine.connect() as conn:
-        query = "select * from keystroke where timestamp like '{}%'".format(str(datetime.datetime.now().date()))
+        query = "select * from keystroke where timestamp like '{}%'".format(
+            str(datetime.datetime.now().date())
+        )
         return pd.read_sql(query, con=conn)
 
+
 def get_total_keystrokes():
-    return data_for_the_day()['total'].sum()
+    return data_for_the_day()["total"].sum()
+
 
 def get_sum_of_all_keypress():
-    print(data_for_the_day().sum().drop(['total', 'timestamp']))
-    return pd.DataFrame(data_for_the_day().sum().drop(['total', 'timestamp']))
+    print(data_for_the_day().sum().drop(["total", "timestamp"]))
+    return (
+        pd.DataFrame(data_for_the_day().sum()).drop(["total", "timestamp"]).transpose()
+    )
+
 
 def serve_layout():
     fig = px.bar(get_sum_of_all_keypress(), barmode="group")
-    return html.Div([
-        html.H1(
-            children='Total keystrokes',
-            style={ 'textAlign': 'center'}
-        ),
-        daq.LEDDisplay(
-            id='my-LED-display',
-            label="",
-            color="#103366",
-            value=get_total_keystrokes(),
-            style={ 'textAlign': 'center'}
-        ),
-        html.Br(), html.Br(),
-        html.Div(children=[
-            html.H1(children='Keystroke graph', style={'textAlign': 'center'}),
-            dcc.Graph(
-                id='example-graph',
-                figure=fig
-            )
-        ])
-    ])
+    return html.Div(
+        [
+            html.H1(children="Total keystrokes", style={"textAlign": "center"}),
+            daq.LEDDisplay(
+                id="my-LED-display",
+                label="",
+                color="#103366",
+                value=get_total_keystrokes(),
+                style={"textAlign": "center"},
+            ),
+            html.Br(),
+            html.Br(),
+            html.Div(
+                children=[
+                    html.H1(children="Keystroke graph", style={"textAlign": "center"}),
+                    dcc.Graph(id="example-graph", figure=fig),
+                ]
+            ),
+        ]
+    )
 
 
 if __name__ == "__main__":
     app.layout = serve_layout
-    app.run_server(host='0.0.0.0', port=8050)
+    app.run_server(host="0.0.0.0", port=8050)
